dd8f16f553985d99e305c3b8e62e275828cb16da
Victoria Artyushkina <victoriaartyushkina@gmail.com>
2023-03-01 23:52:46 +0100

    Add save_model function

--- scripts/helper_functions.py
+++ scripts/helper_functions.py
@@ -162,6 +162,7 @@ def plot_loss_curves(results):
     plt.xlabel("Epochs")
     plt.legend()
 
+
 def pred_and_plot_image(
     model: torch.nn.Module,
     image_path: str,
@@ -280,3 +281,33 @@ def download_data(source: str,
             os.remove(data_path / target_file)
 
     return image_path
+
+
+def save_model(model: torch.nn.Module,
+               target_dir: str,
+               model_name: str):
+    """Saves a PyTorch model to a target directory.
+    Args:
+    model: A target PyTorch model to save.
+    target_dir: A directory for saving the model to.
+    model_name: A filename for the saved model. Should include
+      either ".pth" or ".pt" as the file extension.
+    Example usage:
+    save_model(model=model_0,
+               target_dir="models",
+               model_name="05_going_modular_tingvgg_model.pth")
+    """
+    # Create target directory
+    target_dir_path = Path(target_dir)
+    target_dir_path.mkdir(parents=True,
+                          exist_ok=True)
+
+    # Create model save path
+    assert model_name.endswith(".pth") or model_name.endswith(
+        ".pt"), "model_name should end with '.pt' or '.pth'"
+    model_save_path = target_dir_path / model_name
+
+    # Save the model state_dict()
+    print(f"[INFO] Saving model to: {model_save_path}")
+    torch.save(obj=model.state_dict(),
+               f=model_save_path)
